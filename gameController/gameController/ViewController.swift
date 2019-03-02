65fc3ff7bfb401f38c91bf7a90ed25dcf01b495e
evapphilips <ep1521@nyu.edu>
2019-03-02 16:44:19 -0500

    adding the game controller folder

---
+++ gameController/gameController/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gameController
+//
+//  Created by Eva Philips on 3/2/19.
+//  Copyright © 2019 evaphilips. All rights reserved.
+//
+
+import UIKit
+import Starscream
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
