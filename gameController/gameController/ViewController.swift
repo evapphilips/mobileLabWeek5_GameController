c6ed23eb00862c9aca996b3ada58773abcdc0da7
evapphilips <ep1521@nyu.edu>
2019-03-03 10:41:47 -0500

    added web socket connection code

--- gameController/gameController/ViewController.swift
+++ gameController/gameController/ViewController.swift
@@ -7,15 +7,53 @@
 //
 
 import UIKit
-// include the Cocoa pod info
+// include the Cocoa pod socket library
 import Starscream
 
-class ViewController: UIViewController {
+// direction commands
+enum DirectionCode: String {
+    case up = "0"
+    case right = "1"
+    case down = "2"
+    case left = "3"
+}
+
+class ViewController: UIViewController, WebSocketDelegate {
+    
+    // Object for managing the web socket.
+    var socket: WebSocket?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Web socket setup
+        // URL of the websocket server.
+        let urlString = "wss://gameserver.mobilelabclass.com"
+        // create the websocket
+        socket = WebSocket(url: URL(string: urlString)!)
+        // Assign WebSocket delegate to self
+        socket?.delegate = self
+        // Connect to the web socket
+        socket?.connect()
     }
+    
+    // Web Socket Method
+    func websocketDidConnect(socket: WebSocketClient) {
+        print("socket connected")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        print("socket disconnected", error ?? "No message")
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+        print("socket recieved message", text)
+    }
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+        print("socket recieved data", data)
+    }
+    
 
 
 }
